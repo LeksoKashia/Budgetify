f7407d06fbd80efa815439062a53711e673f4eaf
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2024-02-08 05:50:34 +0400

    getting logged in user info

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User.model';
 import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  user : User;
+  constructor(private authService: AuthService, private userService: UserService){}
+  
+  ngOnInit(): void {
+    this.user = this.userService.getUserInfo();
+  }
 
-  constructor(private authService: AuthService){}
   logout(){
     this.authService.logout();
   }
