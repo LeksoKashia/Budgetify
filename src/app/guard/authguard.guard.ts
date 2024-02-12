4e05ecea5f43b352a07b30e358050f46700218be
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2024-02-12 04:24:43 +0400

    using ngrx state for route guarding

--- src/app/guard/authguard.guard.ts
+++ src/app/guard/authguard.guard.ts
@@ -2,20 +2,30 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
+import { AuthState } from 'src/app/redux/reducers/auth.reducer';
+
+import { isLoggedIn } from '../redux/selectors/auth.selectors'; 
+import { Observable, map } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private store: Store<AuthState>) {}
 
-  canActivate(): boolean {
-    if (this.authService.isLoggedIn()) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-      return false;
-    }
+  canActivate(): Observable<boolean> {
+    return this.store.select(isLoggedIn).pipe(
+      map(isLoggedIn => {
+        if (!isLoggedIn) {
+          this.router.navigate(['/login']);
+          return false;
+        }
+        return true;
+      })
+    );
   }
 }
