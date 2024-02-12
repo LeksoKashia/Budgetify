19525ef7730ecc111135af5ee6ab7c7c5e405d34
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2024-02-12 04:23:08 +0400

    removed local variables and started using NGRX state for data storage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { isLoggedIn } from '../redux/selectors/auth.selectors'; 
+import { AuthState } from '../redux/reducers/auth.reducer';
+import * as AuthActions from '../redux/actions/auth.actions';
+import { clearUser } from '../redux/actions/user.actions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private loggedIn = false;
 
-  constructor() { }
+  constructor(private store: Store<AuthState>) {}
 
   login() {
-    this.loggedIn = true;
+    this.store.dispatch(AuthActions.login());
   }
 
   logout() {
-    this.loggedIn = false;
+    this.store.dispatch(clearUser());
+    this.store.dispatch(AuthActions.logout());
   }
 
-  isLoggedIn(): boolean {
-    return this.loggedIn;
+  isLoggedIn(): Observable<boolean> {
+    return this.store.select(isLoggedIn);
   }
+
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,29 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/User.model';
+import { Store } from '@ngrx/store';
+import { UserState } from '../redux/reducers/user.reducer';
+import * as UserActions from '../redux/actions/user.actions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private user: User; 
-
   private apiServerUrl = 'http://localhost:8080';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private store: Store<UserState>) {}
 
   public getUser(email: string): Observable<User> {
     return this.http.get<User>(`${this.apiServerUrl}/user/find/${email}`);
   }
 
-
-  setUser(user: any) {
-    this.user = user;
+  setUser(user: User) {
+    this.store.dispatch(UserActions.setUser({ user }));
   }
 
   getUserInfo() {
-    return this.user;
+    this.store.dispatch(UserActions.getUserInfo());
   }
-  
 
 }
