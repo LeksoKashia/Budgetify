86e7570eed9763fd2f1446f5892054589782eadd
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2024-02-12 04:19:37 +0400

    auth and user reducers for ngrx implementation

---
+++ src/app/redux/reducers/user.reducer.ts
@@ -0,0 +1,17 @@
+import { createReducer, on } from '@ngrx/store';
+import * as UserActions from '../actions/user.actions';
+import { User } from 'src/app/models/User.model';
+
+export interface UserState {
+  user: User | null;
+}
+
+const initialState: UserState = {
+  user: null
+};
+
+export const userReducer = createReducer(
+  initialState,
+  on(UserActions.setUser, (state, { user }) => ({ ...state, user })),
+  on(UserActions.clearUser, (state) => ({ ...state, user: null })),
+);
