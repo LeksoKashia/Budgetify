dd4216a894c29269e0785315b743f778928eb19b
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2024-02-13 23:52:26 +0400

    registration implemented

---
+++ src/app/components/registration/registration.component.ts
@@ -0,0 +1,53 @@
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent implements OnInit {
+  registrationForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private router: Router) {
+    this.registrationForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid && !this.passwordsDoNotMatch()) {
+      const user = {
+        name: this.registrationForm.value.name,
+        email: this.registrationForm.value.email,
+        password: this.registrationForm.value.password
+      };
+      this.userService.addUser(user).subscribe(
+        (response) => {
+          console.log('User registered successfully:', response);
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          console.error('Error registering user:', error);
+
+        }
+      );
+    }
+  }
+
+  passwordsDoNotMatch(): boolean {
+    const password = this.registrationForm.value.password;
+    const confirmPassword = this.registrationForm.value.confirmPassword;
+    return password !== confirmPassword;
+  }
+}
