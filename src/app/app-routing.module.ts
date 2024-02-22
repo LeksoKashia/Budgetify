9513a6d676700906e9a6acc135ac60d088894c58
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2024-02-23 01:41:19 +0400

    added route for admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './guard/authguard.guard';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent }
+  
 ];
 
 @NgModule({
