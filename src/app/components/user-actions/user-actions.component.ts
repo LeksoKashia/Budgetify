cc4c22f97951c00c7f914476c59d548fe9afa460
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2024-02-28 14:15:46 +0400

    added piggy logic

--- src/app/components/user-actions/user-actions.component.ts
+++ src/app/components/user-actions/user-actions.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
+import { Account } from 'src/app/models/Account.model';
+import { PiggyBank } from 'src/app/models/PiggyBank.model';
+import { PaymentsService } from 'src/app/services/payments.service';
+import { PiggyBankService } from 'src/app/services/piggyBankService/piggy-bank.service';
 
 
 @Component({
@@ -10,30 +14,92 @@ import { filter } from 'rxjs';
 })
 export class UserActionsComponent implements OnInit{
 
+  piggyBanks: PiggyBank[];
   showOverlay: boolean = false;
   formDisplayText : string;
   showAccountAdd: boolean = false;
   showTransactionAdd: boolean = false;
   showPiggyBankAdd: boolean = false;
+  showPiggy: boolean = false;
+  selectedPiggy: any;
+  editPiggyLayout: boolean = false;
+  addMoneyToPiggy: boolean = false;
+  activeCard: Account;
+  @Output() reInitialiseAccount = new EventEmitter<void>();
 
 
-  constructor(private router: Router,  private activatedRoute: ActivatedRoute) { }
+
+  constructor(private router: Router,  private activatedRoute: ActivatedRoute, private paymentService: PaymentsService) { }
   ngOnInit(): void {
-    // this.router.events.pipe(
-    //   filter(event => event instanceof NavigationEnd)
-    // ).subscribe(() => {
-    //   this.showEverything = !this.activatedRoute.snapshot.firstChild?.routeConfig?.path?.includes('movie');
-    //   this.showCategory = !this.activatedRoute.snapshot.firstChild?.routeConfig?.path?.includes('category');
-    // });
+    const storedCard = localStorage.getItem('activeCard');
+    this.activeCard =  JSON.parse(storedCard);
+    this.getPiggyBanks();
   }
 
- toggleOverlay(formName?: string) {
-  this.showOverlay = !this.showOverlay;
-  this.formDisplayText = formName;
+  getPiggyBanks(){
+    if(this.activeCard){
+      this.paymentService.getPiggyBanks().subscribe(
+        (piggyBanks: PiggyBank[]) => {
+          this.piggyBanks = piggyBanks;
+        }
+      )
+    }
+  }
 
-  this.showAccountAdd = formName === "Account";
-  this.showTransactionAdd = formName === "Transaction";
-  this.showPiggyBankAdd = formName === "Piggy Bank";
- }
+  toggleOverlay(formName?: string) {
+    setTimeout(() => {
+      this.getPiggyBanks();
+    }, 200);
+   
+    this.showOverlay = !this.showOverlay;
+    this.formDisplayText = formName;
+
+    this.showAccountAdd = formName === "Account";
+    this.showTransactionAdd = formName === "Transaction";
+    this.showPiggyBankAdd = formName === "Piggy Bank";
+  }
+
+  togglePiggy(piggy?: any, component?: string) : number{
+    this.editPiggyLayout = false;
+    this.addMoneyToPiggy = false;
+    if (piggy) {
+      this.selectedPiggy = piggy;
+    }
+    if(component == 'info'){
+      this.editPiggyLayout = false;
+      return 0;
+    }
+    if(component == "edit"){
+      this.editPiggyLayout = true;
+      return 0;
+    }
+
+    this.showPiggy = !this.showPiggy;
+
+    if (!piggy && !component) {
+      setTimeout(() => {
+        this.getPiggyBanks();
+      }, 200);
+    }
+    
+    return 0;
+
+
+  }
+
+  addToPiggy(piggyBank: PiggyBank){
+    this.editPiggyLayout = false;
+    this.addMoneyToPiggy = !this.addMoneyToPiggy;
+  }
+
+  getRangeBackground(savedAmount: number, goalAmount: number) {
+    const filledPercentage = (savedAmount / goalAmount) * 100;
+    return `linear-gradient(to right, rgb(255, 118, 174) ${filledPercentage}%, white ${filledPercentage}%)`;
+  }
+
+  reInitialise(){
+    this.reInitialiseAccount.emit();
+  }
+
 
 }
