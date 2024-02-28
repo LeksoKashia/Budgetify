8c74bda89a5fc256a78e9b3803c683f81491dcbb
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2024-02-28 14:11:52 +0400

    added function reinitialision logic

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from 'src/app/models/User.model';
 import { UserService } from 'src/app/services/user.service';
+import { AccountsComponent } from '../accounts/accounts.component';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,12 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit{
+  @ViewChild('accountsComponent', { static: false }) accountsComponent: AccountsComponent;
 
   ngOnInit(): void {}
-   
+  reInitialise() {
+    if (this.accountsComponent) {
+      this.accountsComponent.reFetchAccounts();
+    }
+  }
 }
