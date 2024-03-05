409f66c489566d67987f3bf13aed8e2992c78060
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2024-03-05 06:51:56 +0400

    implemented subscriptions logic

--- src/app/components/subscriptions/subscriptions.component.ts
+++ src/app/components/subscriptions/subscriptions.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'src/app/models/Subscription.model';
+import { PaymentsService } from 'src/app/services/payments.service';
 
 @Component({
   selector: 'app-subscriptions',
   templateUrl: './subscriptions.component.html',
   styleUrls: ['./subscriptions.component.scss']
 })
-export class SubscriptionsComponent {
+export class SubscriptionsComponent implements OnInit {
+  subscriptions: Subscription[];
+  showOverlay: boolean = false;
 
+  constructor(private paymentService: PaymentsService) {}
+
+  ngOnInit() {
+    this.fetchSubscriptions();
+  }
+
+  toggleOverlay(s?: string) {
+    if(s){
+      setTimeout(() => {
+        this.fetchSubscriptions();
+      }, 200);
+    }
+    this.showOverlay = !this.showOverlay;
+  }
+
+  fetchSubscriptions() {
+    this.paymentService.getSubscriptions().subscribe(
+      (subscriptions: any) => {
+        this.subscriptions = subscriptions;
+      },
+      (error) => {
+        console.error('Error fetching subscriptions:', error);
+      }
+    );
+  }
+
+  getFirstWord(str: string): string {
+    if (!str) return ''; // Handle empty string case
+    return str.split(' ')[0]; // Split by space and return the first element
+  }
 }
