b48f2104de083e843e205e196af69e8422bd4186
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2024-03-05 06:58:51 +0400

    created subscriptions-add layout and logic

---
+++ src/app/components/forms/add/subscription-add/subscription-add.component.ts
@@ -0,0 +1,69 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { PaymentsService } from 'src/app/services/payments.service';
+import { Account } from 'src/app/models/Account.model';
+import { Subscription } from 'src/app/models/Subscription.model';
+import { SubscriptionService } from 'src/app/services/subscriptionService/subscription.service';
+
+@Component({
+  selector: 'app-subscription-add',
+  templateUrl: './subscription-add.component.html',
+  styleUrls: ['./subscription-add.component.scss', '../../add/account-add/account-add.component.scss']
+})
+export class SubscriptionAddComponent {
+  @Output() closeForm = new EventEmitter<void>();
+
+  subscriptionForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private subscriptionService: SubscriptionService,
+    private paymentService: PaymentsService
+  ) {
+    this.subscriptionForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      categories: ['', Validators.required],
+      amount: ['', Validators.required],
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  }
+
+  close() {
+    this.closeForm.emit();
+  }
+
+  onSubmit() {
+    this.closeForm.emit();
+    if (this.subscriptionForm.valid) {
+      const storedCard = localStorage.getItem('activeCard');
+      const activeCard: Account= JSON.parse(storedCard);
+      if (activeCard && activeCard.id) {
+        const subscriptionData: Subscription = {
+          title: this.subscriptionForm.value.title,
+          categories: this.subscriptionForm.value.categories,
+          amount: this.subscriptionForm.value.amount,
+          startDate: this.subscriptionForm.value.startDate,
+          endDate: this.subscriptionForm.value.endDate,
+          description: this.subscriptionForm.value.description,
+          account: activeCard
+        };
+
+        this.subscriptionService.addSubscription(subscriptionData).subscribe(
+          (newSubscription: Subscription) => {
+            console.log('Subscription added successfully:', newSubscription);
+          },
+          (error) => {
+            console.error('Error adding subscription:', error);
+          }
+        );
+      } else {
+        console.error('User information not found.');
+      }
+    } else {
+      console.log('Please fill in all fields.');
+    }
+  }
+}
