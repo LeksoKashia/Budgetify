5e3f6860dc919fdfc94e655a37f79fea89a45f6e
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2024-03-19 04:54:53 +0400

    small modifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule, isDevMode } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -15,12 +16,12 @@ import { userReducer } from './redux/reducers/user.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { AccountAddComponent } from './components/forms/add/account-add/account-add.component';
-import { TransactionAddComponent } from './components/forms/add/transaction-add/transaction-add.component';
 import { PiggyBankAddComponent } from './components/forms/add/piggy-bank-add/piggy-bank-add.component';
 import { AccountInfoComponent } from './components/forms/info/account-info/account-info.component';
 import { AccountEditComponent } from './components/forms/edit/account-edit/account-edit.component';
 import { TransactionEditComponent } from './components/forms/edit/transaction-edit/transaction-edit.component';
 import { TransactionInfoComponent } from './components/forms/info/transaction-info/transaction-info.component';
+import { TransactionAddComponent } from './components/forms/add/transaction-add/transaction-add.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { PiggyBankInfoComponent } from './components/forms/info/piggy-bank-info/piggy-bank-info.component';
 import { PiggyBankEditComponent } from './components/forms/edit/piggy-bank-edit/piggy-bank-edit.component';
@@ -58,9 +59,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SubscriptionAddComponent,
     SubscriptionInfoComponent,
     SubscriptionEditComponent,
-    ObligatoryAddComponent
+    ObligatoryAddComponent,
+    TransactionAddComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
