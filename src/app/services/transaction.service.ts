0821eb639290a0b644a9e0b20ffb3181cf233627
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2024-04-01 07:28:02 +0400

    added requests for trasnaction images and categories

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -3,13 +3,24 @@ import { Injectable } from '@angular/core';
 import { Transaction } from 'src/app/models/transaction.model';
 import { Constants } from 'src/app/constants/constants';
 import { Observable } from 'rxjs';
+import { TransactionCategory } from '../models/transaction-category.model';
+import { ImageModel } from '../models/image.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
   private apiServerUrl = Constants.apiUrl;  
+
   constructor(private http: HttpClient) {}
+
+  getCategories(transactionId: number): Observable<TransactionCategory[]> {
+    return this.http.get<TransactionCategory[]>(`${this.apiServerUrl}/transaction/categories/${transactionId}`);
+  }
+
+  getImages(transactionId: number): Observable<ImageModel[]> {
+    return this.http.get<ImageModel[]>(`${this.apiServerUrl}/transaction/images/${transactionId}`);
+  }
 
   addTransaction(transaction: Transaction): Observable<Transaction> {
     return this.http.post<Transaction>(`${this.apiServerUrl}/transaction/add`,transaction);
@@ -19,7 +30,7 @@ export class TransactionService {
     return this.http.put<Transaction>(`${this.apiServerUrl}/transaction/update`,transaction);
   }
 
-  deleteAccount(transactionId: number): Observable<void> {
+  deleteTransaction(transactionId: number): Observable<void> {
     return this.http.delete<void>(`${this.apiServerUrl}/transaction/delete/${transactionId}`);
   }
 }
